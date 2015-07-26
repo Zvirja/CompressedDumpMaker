0a481ead4d8a868e8444d14aeadb2abbfdac6985
Alex Povar <user.home.0000@gmail.com>
2015-07-27 01:51:10 +0300

    Added API export file

--- CompressedDumpMaker/CompressedDumpMaker.cpp
+++ CompressedDumpMaker/CompressedDumpMaker.cpp
@@ -1,22 +1,10 @@
-// CompressedDumpMaker.cpp : Defines the exported functions for the DLL application.
-//
-
 #include "stdafx.h"
+
 #include "CompressedDumpMaker.h"
+#include "DumpCreator.h"
 
-
-// This is an example of an exported variable
-COMPRESSEDDUMPMAKER_API int nCompressedDumpMaker=0;
-
-// This is an example of an exported function.
-COMPRESSEDDUMPMAKER_API int fnCompressedDumpMaker(void)
+bool createDumpFile(unsigned long processId, std::wstring&& fileNameNoExt)
 {
-	return 42;
-}
-
-// This is the constructor of a class that has been exported.
-// see CompressedDumpMaker.h for the class definition
-CCompressedDumpMaker::CCompressedDumpMaker()
-{
-	return;
+	DumpCreator dumpCreator(processId, std::move(fileNameNoExt));
+	return dumpCreator.CreateDump();
 }
