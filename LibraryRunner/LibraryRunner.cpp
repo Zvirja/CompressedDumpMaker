8bc684534550e69300a385d1b570f2cd956d2e56
Alex Povar <user.home.0000@gmail.com>
2015-07-27 12:08:10 +0300

    Modified runner to get path as arguments

--- LibraryRunner/LibraryRunner.cpp
+++ LibraryRunner/LibraryRunner.cpp
@@ -3,12 +3,32 @@
 
 #include "stdafx.h"
 #include "../CompressedDumpMaker/CompressedDumpMaker.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	createDumpFile(10992, std::wstring(L"C:\\Users\\Zvirja\\Documents\\Sources\\CompressedDumpMaker\\LibraryRunner\\TestOutput\\testFile"));
+	if (argc < 3)
+	{
+		wcout << "Missed args" << endl;
+		return 1;
+	}
+
+	auto pid = std::stoi(argv[1]);
+	wstring outputFileName = wstring(argv[2]);
+	
+
+	auto startTime = chrono::high_resolution_clock::now();
 
 
+	bool success = createDumpFile(pid, outputFileName);
+
+	auto elapsedMsTime = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - startTime).count();
+
+	wcout << "Success: " << success << endl;
+	wcout << "Elapsed ms time: " << elapsedMsTime << endl;
 
 	return 0;
 }
